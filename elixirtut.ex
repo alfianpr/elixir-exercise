f618ad915b296d02d924f8895f798f3cfa3f5fea
alfianpr <alfian.pratama@kalbenutritionals.com>
2023-09-30 01:01:15 +0700

    update

---
+++ elixirtut.ex
@@ -0,0 +1,11 @@
+ defmodule M do
+  def main do
+    # name = IO.gets("test the elixir") |> String.trim
+    IO.puts "testing the"
+  end
+
+  def data_stuff do
+    my_int = 123
+    IO.puts "Integer #{is_integer(my_int)}"
+  end
+end
